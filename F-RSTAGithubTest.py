0a4f0807f76b83e2c3e8836c6e74e6ac28c6b24b
Frej Lars Svedenmark <08svefre@taby.se>
2025-11-05 12:47:23 +0100

    Är du sigma?

---
+++ F-RSTAGithubTest.py
@@ -0,0 +1,10 @@
+print ("Hej")
+
+x = int(input("""1:Säg hej tillbaka
+               2: Säg inget tillbaka"""))
+
+if x == 1:
+    print(":)")
+else:
+    for i in range (10000):
+        print ("bajs")
